bc04ca9bc46318ce35f07d8384fb6238bfec1d32
enrouteeee <enrouteeee@gmail.com>
2022-01-03 22:29:52 +0900

    oauth2

---
+++ security/src/main/java/com/example/security/controller/TestApi.java
@@ -0,0 +1,15 @@
+package com.example.security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class TestApi {
+
+    @GetMapping("/test")
+    public String test(){
+        return "Hello World";
+    }
+}
